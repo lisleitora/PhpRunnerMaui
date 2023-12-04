2fcb4833e25bbfc7470f14b61bad77132495ccb3
Pablo <pablo.erick@xpand-it.com>
2023-12-04 16:13:47 +0000

    news

---
+++ PhpRunnerMaui.Sample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace PhpRunnerMaui.Sample;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        PhpRunnerMaui.ServerApi = "http://********************";
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        List<ClassAll> classAlls = await PhpRunnerMaui.Service.List<ClassAll>();
+        foreach (var item in classAlls)
+        {
+            System.Diagnostics.Debug.WriteLine("::::: " + item.teacher);
+        }
+    }
+
+    public class ClassAll
+    {
+        public string id { get; set; }
+        public string curse_id { get; set; }
+        public string company_id { get; set; }
+        public string student_id { get; set; }
+        public string teacher_id { get; set; }
+        public string days { get; set; }
+        public string duration { get; set; }
+        public string evaluation_period { get; set; }
+        public string hours { get; set; }
+        public string active { get; set; }
+        public string company { get; set; }
+        public string teacher { get; set; }
+        public string student { get; set; }
+        public string curse { get; set; }
+    }
+}
---
+++ PhpRunnerMaui.Sample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhpRunnerMaui.Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
